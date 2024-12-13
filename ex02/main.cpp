fd0bfd3cb859d0f85ae72c57bb1ada828faecdd1
Silva5525 <132822583+Silva5525@users.noreply.github.com>
2024-12-13 12:30:44 +0100

    list test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wdegraf <wdegraf@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 13:39:48 by wdegraf           #+#    #+#             */
-/*   Updated: 2024/12/13 12:16:05 by wdegraf          ###   ########.fr       */
+/*   Updated: 2024/12/13 12:29:55 by wdegraf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,44 @@ int	main(void)
 
 	return (0);
 }
+
+// #include <list>
+// /// @brief test main with list
+// int main(void)
+// {
+// 	std::list<int> mlist;
+
+// 	mlist.push_back(5);
+// 	mlist.push_back(17);
+
+// 	std::cout << "Top (last element): " << mlist.back() << std::endl;
+
+// 	mlist.pop_back();
+// 	std::cout << "Size after pop: " << mlist.size() << std::endl;
+
+// 	mlist.push_back(3);
+// 	mlist.push_back(5);
+// 	mlist.push_back(737);
+// 	mlist.push_back(0);
+
+// 	std::cout << "List contents using iterators:" << std::endl;
+// 	std::list<int>::iterator it = mlist.begin();
+// 	std::list<int>::iterator ite = mlist.end();
+// 	while (it != ite) {
+// 		std::cout << *it << std::endl;
+// 		++it;
+// 	}
+
+// 	std::cout << "List contents using reverse iterators:" << std::endl;
+// 	std::list<int>::reverse_iterator rit = mlist.rbegin();
+// 	std::list<int>::reverse_iterator rite = mlist.rend();
+// 	while (rit != rite) {
+// 		std::cout << *rit << std::endl;
+// 		++rit;
+// 	}
+
+// 	return (0);
+// }
 
 /// Subjekt main example
 // int main()
